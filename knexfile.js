3706b2e2a04a3cb1772bba530b4b80fe18e73d25
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-18 19:20:36 -0400

    mvp

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,22 @@
-// Update with your config settings.
+
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './database/auth.db3'
-    },
+    client: "sqlite3",
     useNullAsDefault: true,
-    migrations: {
-      directory: './database/migrations' 
-    },
-    seeds: {
-      directory: './database/seeds' 
+    connection: {
+      filename: "./database/auth.db3",
     },
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      }
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
     },
   },
-
 };
--- server.js
+++ server.js
@@ -1,22 +1,37 @@
-const express = require('express');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
+
+const usersRouter = require("./users/users-router");
+const authRouter = require("./auth/router");
 
 const server = express();
 
-const authRouter = require('./router/auth-router.js');
+const sessionConfig = {
+  cookie: {
+    maxAge: 1000 * 60 * 60, // one hour in milliseconds
+    secure: process.env.SECURE_COOKIE || false, // send the cookie only over https, true in production
+    httpOnly: true, // true means client JS cannot access the cookie
+  },
+  resave: false,
+  saveUninitialized: process.env.USER_ALLOWED_COOKIES || true,
+  name: "monster",
+  secret: process.env.COOKIE_SECRET || "keepitsecret,keepitsafe!",
+};
 
-const helmet = require("helmet");
-
+// create a session and send a cookie back (the cookie will store the session id)
+server.use(session(sessionConfig)); // turn on sessions for the API
 
 server.use(helmet());
-
-
-
 server.use(express.json());
-server.use('/api/users', authRouter);
-//line 15 is starting point for site to use in postman
+server.use(cors());
 
-server.get('/', (req, res) => {
-    res.send(`<h2>Let's write some functioning stuff for this last week of backend!</h2>`);
-  });
+server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
+
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
+});
 
 module.exports = server;
