1b3aef511cf72bef18f8cdd77605e905c9225c5a
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-18 16:24:31 -0400

    bcryptjs installed

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './database/projects.db3'
+      filename: './database/auth.db3'
     },
     useNullAsDefault: true,
     migrations: {
